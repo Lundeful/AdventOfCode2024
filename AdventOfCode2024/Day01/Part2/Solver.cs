15315a4e93ffd8f3714c6a1cfae99b66ad2391d3
Christoffer Lund <chrislund90@gmail.com>
2024-12-01 17:17:12 +0100

    Solve Day 02 Part 02

--- AdventOfCode2024/Day01/Part2/Solver.cs
+++ AdventOfCode2024/Day01/Part2/Solver.cs
@@ -6,6 +6,29 @@ public sealed class Solver : ISolver
 {
     public string Solve(string[] input)
     {
-        return string.Empty;
+        var leftList = new List<int>();
+        var rightList = new List<int>();
+
+        foreach (var line in input)
+        {
+            var parts = line.Split(" ");
+            leftList.Add(int.Parse(parts[0]));
+            rightList.Add(int.Parse(parts[^1]));
+        }
+
+        leftList.Sort();
+        rightList.Sort();
+
+        if (leftList.Count != rightList.Count)
+            throw new InvalidOperationException("Side lengths do not match");
+
+        var result = 0;
+        foreach (var left in leftList)
+        {
+            var left1 = left;
+            result += rightList.Count(right => right == left1) * left;
+        }
+
+        return result.ToString();
     }
 }
