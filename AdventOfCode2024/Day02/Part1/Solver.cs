a6c6df59fb1bbc5191d6d4b100dd168b5d1252da
Christoffer Lund <chrislund90@gmail.com>
2024-12-02 06:23:36 +0100

    Solved Day 2 Part 1

--- AdventOfCode2024/Day02/Part1/Solver.cs
+++ AdventOfCode2024/Day02/Part1/Solver.cs
@@ -6,6 +6,46 @@ public sealed class Solver : ISolver
 {
     public string Solve(string[] input)
     {
-        return string.Empty;
+        var safeReports = 0;
+        foreach (var line in input)
+        {
+            var levels = line.Split(' ');
+            var isSafe = true;
+            var i = 0;
+            var originalDirection = int.Parse(levels[0]) - int.Parse(levels[1]) > 0;
+            while (isSafe && i + 1 < levels.Length)
+            {
+                var level = int.Parse(levels[i]);
+                var nextLevel = int.Parse(levels[i + 1]);
+                if (level == nextLevel)
+                {
+                    isSafe = false;
+                    break;
+                }
+
+                var diff = level - nextLevel;
+                if (Math.Abs(diff) is < 1 or > 3)
+                {
+                    isSafe = false;
+                    break;
+                }
+
+                var currentDirection = diff > 0;
+                if (originalDirection != currentDirection)
+                {
+                    isSafe = false;
+                    break;
+                }
+
+                i++;
+            }
+
+            if (isSafe)
+            {
+                safeReports++;
+            }
+        }
+
+        return safeReports.ToString();
     }
 }
